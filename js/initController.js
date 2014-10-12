89b096a28a7fb0b610208ab9374a7bdf5de6e872
Logan Collingwood <logan.collingwood@gmail.com>
2014-10-11 22:32:53 -0700

    Working on implementing controller gifs per page
    Using api/frameData.php to answer a jQuery ajax request, and hardcoding
    json data :-1:

---
+++ js/initController.js
@@ -0,0 +1,11 @@
+
+var first = getUrlVars()["id"];
+var second = getUrlVars()["page"];
+
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
+}
