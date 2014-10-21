1fa69ae3ef3fd7539ffd220c035ddb18d63ef2a3
Marco Salazar de Leon <msalazardeleon@gmail.com>
2014-10-20 21:20:10 -0700

    Styled and structured

--- submit-nav.php
+++ submit-nav.php
@@ -9,23 +9,86 @@
       </button>
       <a class='navbar-brand heddur' href='/'>smash lounge&nbsp;&nbsp;&nbsp;&nbsp;<i class='fa fa-gamepad fa-1x'></i></a>
     </div>
+    <div class="navbar-right dropdown submit-wrapper">
+      <a class='btn btn-navbar navbar-btn dropdown-toggle' data-toggle="dropdown" href="#">submit</a>
+      <div class="dropdown-menu" role="menu">
+        <form class="form">
+          <fieldset>
+            <label for="url">GfyCat URL</label>
+            <input type="text" class="form-control" name="url" placeholder="LusciousFattyNarwhal"/>
+          </fieldset>
+          <fieldset>
+            <label for="source">Original Source</label>
+            <input type="text" class="form-control" maxlength="100" name="source" placeholder="Big House 4"/>
+          </fieldset>
+          <fieldset>
+            <label for="description">Description</label>
+            <input type="text" class="form-control" maxlength="100" name="description" placeholder="Mew2King Ken Comboes a Scrub"/>
+          </fieldset>
+          <input type="submit" class="btn btn-default" name="submit" value="submit"/>
+        </form>
+      </div>
+    </div>
     <div class='navbar-collapse collapse'>
       <ul class='nav navbar-nav navbar-right'>
-          <li><a href='https://www.twitter.com/thesmashlounge'><i class='fa fa-twitter'></i></a></li>
-          <li><a href='https://www.facebook.com/SmashLounge'><i class='fa fa-facebook'></i></a></li>
-          <li><a href='http://www.reddit.com/r/smashlounge'><i class='fa fa-reddit'></i></a></li>
-          <li><a href='http://www.github.com/logancollingwood/smashlounge'><i class='fa fa-github-alt'></i></a></li>
-          <li><a href='/api/docs'>api</a></li>
-          <li><a href='/donate.php'>donate</a></li>
-          <li><a href='/about.html'>about</a></li>
-            <?php if ($extra == 'logout'): ?>
-              <li><a href='static/logout.php'>logout</a></li>
-            <?php else if ($extra =='register'): ?>
-              <li><a href='/register'>register</a></li>
-            <?php else if ($extra == 'update'): ?>
-              <li><a href='/update'>update</a></li>
-            <?php endif; ?>
+        <li><a href='https://www.twitter.com/thesmashlounge'><i class='fa fa-twitter'></i></a></li>
+        <li><a href='https://www.facebook.com/SmashLounge'><i class='fa fa-facebook'></i></a></li>
+        <li><a href='http://www.reddit.com/r/smashlounge'><i class='fa fa-reddit'></i></a></li>
+        <li><a href='http://www.github.com/logancollingwood/smashlounge'><i class='fa fa-github-alt'></i></a></li>
+        <li><a href='/api/docs'>api</a></li>
+        <li><a href='/donate.php'>donate</a></li>
+        <li><a href='/about.html'>about</a></li>
+        <?php if ($extra == 'logout'): ?>
+          <li><a href='static/logout.php'>logout</a></li>
+        <?php elseif ($extra =='register'): ?>
+          <li><a href='/register'>register</a></li>
+        <?php elseif ($extra == 'update'): ?>
+          <li><a href='/update'>update</a></li>
+        <?php endif; ?>
       </ul>
     </div>
   </div>
 </div>
+<style>
+  .btn-navbar {
+    margin-left: 1em;
+    margin-right: 1em;
+    padding: 2px 12px;
+    font-size: 20px;
+    color: #FFF;
+    background: #007E8E;
+  }
+
+  .btn-navbar:hover,
+  .btn-navbar:active,
+  .btn-navbar:focus {
+    color: #FFF;
+  }
+
+  .navbar .heddur:hover .submit-wrapper {
+    color: #FFF;
+  }
+
+  .submit-wrapper .dropdown-menu {
+    background: #0097a7;
+  }
+
+  .submit-wrapper form {
+    padding: 1em 2em;
+    min-width: 320px;
+  }
+
+  .submit-wrapper fieldset {
+    margin-bottom: 1.5em;
+  }
+
+  .submit-wrapper label,
+  .submit-wrapper input {
+    text-align: center;
+  }
+
+  .submit-wrapper .btn-default {
+    color: #FFF;
+    background: #007E8E;
+  }
+</style>
