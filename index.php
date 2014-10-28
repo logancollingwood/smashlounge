6507078853c6fdfcf104459906a29a252427f827
Logan Collingwood <logan.collingwood@gmail.com>
2014-10-27 17:19:24 -0700

    Homepage toggler fix

--- index.php
+++ index.php
@@ -179,13 +179,7 @@ Questions?
       </div>
     </div>
 
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <script src="js/jquery.fitvids.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/toggler.js"></script>
     <script>
        $(document).ready(function(){
           // Target your .container, .wrapper, .post, etc.
