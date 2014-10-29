1510e5c4afe6d1a226f28c0182ce47d90882569e
logancollingwood <logan6694@gmail.com>
2014-10-28 17:20:24 -0700

    Fixing attending
    Adding eventbrite for future tournaments and stuff.

--- techs/initAttending.php
+++ techs/initAttending.php
@@ -17,10 +17,12 @@ function makeEventSignup($event) {
 	echo "<div class='panel panel-default'>";
     echo "	<div class='panel-heading'><a href='/upcoming?tournament=" . urlencode($event['title']) . "'>" . $event['title'] . "</a><br><small><a href='https://www.facebook.com/events/" . $event['fb_id'] . "'>Facebook</a></small></div>";
     echo " 	<div class='panel-body form'>";
-    echo "  	<iframe src='https://docs.google.com/forms/d/1ZEc2zxEEeTwBUNPnvYVVSGLEOp_Nn1LarvaisOPtwWc/viewform?embedded=true' width='760' height='500' frameborder='0' marginheight='0' marginwidth='0'>Loading...</iframe>";
+    echo "  <iframe src='https://docs.google.com/forms/d/1ZEc2zxEEeTwBUNPnvYVVSGLEOp_Nn1LarvaisOPtwWc/viewform?embedded=true' width= '760' height='500' frameborder='0' marginheight='0' marginwidth='0'>Loading...</iframe>";
     echo "  </div>";
     echo "	<div class='panel-footer'>";
-
+    echo "    <div style='width:100%; text-align:left;' >";
+    echo "      <iframe src='http://www.eventbrite.com/tickets-external?eid=13973909339&ref=etckt&v=2' frameborder='0' height='256' width='100%' vspace='0' hspace='0' marginheight='5' marginwidth='5' scrolling='auto' allowtransparency='true'></iframe>";
+    echo "    </div>";
     echo "  </div>";
     echo "</div>";
 }
