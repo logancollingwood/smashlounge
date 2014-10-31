b4f86a3b8a5f8bef7f461051c57270e12caafa31
Marco Salazar de Leon <msalazardeleon@gmail.com>
2014-10-30 21:30:15 -0700

    Created basic admin functionality for demo purposes

--- admin.php
+++ admin.php
@@ -1,5 +1,7 @@
 <?php 
-  $success=$_GET['success']; 
+
+  require_once ('techs/init.php');
+
 ?>
 
 <!DOCTYPE html>
@@ -15,7 +17,8 @@
     <title>Signin Template for Bootstrap</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
     <link href="css/signin.css" rel="stylesheet">
@@ -32,23 +35,114 @@
 
   <body>
 
-    <div class="container">
+    <div class="container">  
+      <?php if ( ! Sentry::check()): ?>
+        <!-- Not Logged In -->
+        <form class="form-signin" role="form" action='techs/auth.php' method='post'>
+          <h2 class="form-signin-heading">Please sign in</h2>
+          <input name='myemail' type="email" class="form-control" placeholder="Email address" required autofocus>
+          <input name='mypass' type="password" class="form-control" placeholder="Password" required>
+          <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+        </form>
+      <?php else: ?>
+        
+        <?php require_once ('techs/initAdmin.php'); ?>
 
-      <form class="form-signin" role="form" action='techs/auth.php' method='post'>
-        <h2 class="form-signin-heading">Please sign in</h2>
-        <input name='myemail' type="email" class="form-control" placeholder="Email address" required autofocus>
-        <input name='mypass' type="password" class="form-control" placeholder="Password" required>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-      </form>
-      <?php
-      if ($success == 'false')
-        echo "<div class='alert alert-danger'>Incorrect login, please try again.</div>";
-      ?>
+        <!-- Logged In -->
+        <div class="management">
+          <header>
+            <h1>Approve / Delete Gifs</h1>
+          </header>
+
+          <?php if (!$result = $mysqli->query($query)): ?>
+            <div class="adminError">
+              <h2>There was a database error.</h2>
+              <p><?php echo $mysqli->error; ?></p>
+            </div>
+          <?php else: ?>
+            <table class="table">
+              <tr>
+                <th>Gfycat URL</th>
+                <th>Original Source</th>
+                <th>Description</th>
+                <th>Approve</th>
+                <th>Delete</th>
+              </tr>
+              <?php foreach ($result as $row): ?>
+                <tr class="submission" data-id="<?php echo $row['id']; ?>">
+                  <td>
+                    <a class="modal-link" href="#">
+                      <?php echo $row['url']; ?>
+                    </a>
+                  </td>
+                  <td><?php echo $row['source']; ?></td>
+                  <td><?php echo $row['description']; ?></td>
+                  <td><a class="approve-link" href="#"><i class="fa fa-check-circle"></i></a></td>
+                  <td><a class="delete-link" href="#"><i class="fa fa-times-circle"></i></a></td>
+                </tr>
+              <?php endforeach; ?>
+            </table>
+          <?php endif; ?>
+        </div>
+      <?php endif; ?>
     </div> <!-- /container -->
 
+    <!-- Modal -->
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+            <h4 class="modal-title" id="myModalLabel">Gfycat Preview</h4>
+          </div>
+          <div class="modal-body">
+            <h1 class="gfycat-target">...</h1>
+          </div>
+        </div>
+      </div>
+    </div>
 
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
+    <script src="//code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+    
+    <script>
+      $(document).ready(function() {
+        $('.modal-link').on('click', function(event) {
+          // Prevent Default Behavior
+          event.preventDefault();
+
+          // Set to modal-link text
+          var gfyCatURL = $(this).text();
+
+          // Change gfycat text
+          $('#myModal .gfycat-target').text(gfyCatURL);
+
+          // Show Modal Window
+          $('#myModal').modal('show');
+        });
+
+        $('.approve-link').on('click', function(event) {
+          // Prevent Default Behavior
+          event.preventDefault();
+
+          var $approveLink = $(this);
+
+          $approveLink.closest('.submission').remove();
+        });
+
+        $('.delete-link').on('click', function(event) {
+          // Prevent Default Behavior
+          event.preventDefault();
+
+          var $deleteLink = $(this);
+
+          $deleteLink.closest('.submission').remove();
+        });
+      });
+    </script>
+
   </body>
 </html>
