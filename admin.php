ec19250eb095f1754e9f1929ff223036f23d22a5
logancollingwood <logan6694@gmail.com>
2014-10-30 21:45:09 -0700

    Fixing login
    for admin page

--- admin.php
+++ admin.php
@@ -40,8 +40,8 @@
         <!-- Not Logged In -->
         <form class="form-signin" role="form" action='login.php?redirect=admin' method='post'>
           <h2 class="form-signin-heading">Please sign in</h2>
-          <input name='myemail' type="email" class="form-control" placeholder="Email address" required autofocus>
-          <input name='mypass' type="password" class="form-control" placeholder="Password" required>
+          <input name='email' id='email' type="email" class="form-control" placeholder="Email address" required autofocus>
+          <input name='password' id='password' type="password" class="form-control" placeholder="Password" required>
           <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
         </form>
       <?php else: ?>
