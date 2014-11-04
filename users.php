f81122324fa1e5ac5da165f006c9660104050b81
logancollingwood <logan6694@gmail.com>
2014-11-03 20:34:17 -0800

    Fixing js

--- users.php
+++ users.php
@@ -247,53 +247,46 @@ Questions?
     <script src="/js/jquery.sticky.js"></script>
     <script src="/js/mapInitUser.js"></script>
     <script>
-    $(document).ready(function(){
-      //RESPONSIVE YOUTUBE VOD
+
+    $(document).ready(function() {  
       $(".youtube").fitVids();
 
-      //For user search
-      $('div.icon').click(function(){
-          $('input#search').focus();
-      });
 
-
-
-      $("input#search").on("keyup", function(e) {
-          // Set Timeout
-          clearTimeout($.data(this, 'timer'));
-
-          // Set Search String
-          var search_string = $(this).val();
-
-          // Do Search
-          if (search_string == '') {
-              $("ul#results").fadeOut();
-              $('h4#results-text').fadeOut();
-          }else{
-              $("ul#results").fadeIn();
-              $('h4#results-text').fadeIn();
-              $(this).data('timer', setTimeout(search, 100));
-          };
-      });
-
+      // Live Search
       // On Search Submit and Get Results
       function search() {
-          console.log('searching');
-          var query_value = $('input#search').val();
-          $('b#search-string').html(query_value);
-          if(query_value !== ''){
-              $.ajax({
-                  type: "POST",
-                  url: "techs/search.php",
-                  data: { query: query_value },
-                  cache: false,
-                  success: function(html){
-                    console.log('success');
-                      $("ul#results").html(html);
-                  }
-              });
-          }return false;
+        var query_value = $('input#search').val();
+        $('b#search-string').html(query_value);
+        if(query_value !== ''){
+          $.ajax({
+            type: "POST",
+            url: "search.php",
+            data: { query: query_value },
+            cache: false,
+            success: function(html){
+              $("ul#results").html(html);
+            }
+          });
+        }return false;    
       }
+
+      $("input#search").live("keyup", function(e) {
+        // Set Timeout
+        clearTimeout($.data(this, 'timer'));
+
+        // Set Search String
+        var search_string = $(this).val();
+
+        // Do Search
+        if (search_string == '') {
+          $("ul#results").fadeOut();
+          $('h4#results-text').fadeOut();
+        }else{
+          $("ul#results").fadeIn();
+          $('h4#results-text').fadeIn();
+          $(this).data('timer', setTimeout(search, 100));
+        };
+      });
 
     });
     </script>
