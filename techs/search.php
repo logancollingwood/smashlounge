8c40b9111faee8c44c3b5a4b06112462a7d40931
logancollingwood <logan6694@gmail.com>
2014-11-03 20:25:14 -0800

    Fixing search :O

--- techs/search.php
+++ techs/search.php
@@ -7,63 +7,63 @@
 	  exit();
 	}
 
-	// Define Output HTML Formatting
+	// Define Output HTML Formating
 	$html = '';
-	$html .= '<ul><li class="result">';
-	$html .= '<a href="urlString" target="_blank">';
-	$html .= '</a><h3>nameString</h3>';
+	$html .= '<li class="result">';
+	$html .= '<a target="_blank" href="urlString">';
+	$html .= '<h3>nameString</h3>';
+	$html .= '</a>';
 	$html .= '</li>';
-	$html .= '</ul>';
+
 
 	// Get Search
 	$search_string = preg_replace("/[^A-Za-z0-9]/", " ", $_GET['query']);
 	$search_string = $mysqli->real_escape_string($search_string);
 	// Check Length More Than One Character
 	if (strlen($search_string) >= 1 && $search_string !== ' ') {
+		echo $search_string;
+
+		// Build Query
+		$query = 'SELECT * FROM users 
+		WHERE username 
+		LIKE "%'.$search_string.'%" 
+		OR name LIKE "%'.$search_string.'%"';
+
+		// Do Search
+		$result = $mysqli->query($query);
+		while($results = $result->fetch_array()) {
+		    $result_array[] = $results;
+		}
+
+		// Check If We Have Results
+		if (isset($result_array)) {
+		    foreach ($result_array as $result) {
+		        // Output
+        		$display_name = preg_replace("/".$search_string."/i", "<strong class='highlight'>".$search_string."</strong>", $result['username']);
+				$display_url = "\/users/" . $result['username'];
+
+
+				// Insert Name
+				$output = str_replace('nameString', $display_name, $html);
+
+				// Insert URL
+				$output = str_replace('urlString', $display_url, $output);
+
+				// Output
+				echo($output);
+		    }
+		}else{
+	    	// Format No Results Output
+			$output = str_replace('urlString', 'javascript:void(0);', $html);
+			$output = str_replace('nameString', '<b>No Results Found.</b>', $output);
+			$output = str_replace('functionString', 'Sorry :(', $output);
+
+			// Output
+			echo($output);
+		}
+
+
 	}
-	echo $search_string;
-	
-	// Build Query
-	$query = 'SELECT * FROM users 
-	WHERE username 
-	LIKE "%'.$search_string.'%" 
-	OR name LIKE "%'.$search_string.'%"';
-
-	// Do Search
-	$result = $mysqli->query($query);
-	while($results = $result->fetch_array()) {
-	    $result_array[] = $results;
-	}
-
-	// Check If We Have Results
-	if (isset($result_array)) {
-	    foreach ($result_array as $result) {
-	        // Output
-	        print_r($result);
-	    }
-	}else{
-	    // Output
-	    echo('Nothing to display.');
-	}
 
 
-	$display_name = preg_replace("/".$search_string."/i", "<strong class='highlight'>".$search_string."</strong>", $result['username']);
-	$display_url = "\/users/" . $result['username'];
-
-
-	// Insert Name
-	$output = str_replace('nameString', $display_name, $html);
-
-	// Insert URL
-	$output = str_replace('urlString', $display_url, $output);
-
-	// Output
-	echo($output);
-
-	// Format No Results Output
-	$output = str_replace('urlString', 'JavaScript:void(0);', $html);
-	$output = str_replace('nameString', 'No Results Found.', $output);
-
-	// Output
-	echo($output);
 ?>
