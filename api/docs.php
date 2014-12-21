274cc0061a7de187e14d7aff94aec4f21bf08706
logancollingwood <logan6694@gmail.com>
2014-12-21 14:05:06 -0800

    Fixed

--- api/docs.php
+++ api/docs.php
@@ -13,7 +13,8 @@
   //Defines the amount of API response examples to render in documentation
   $examples = 2;
   $counter = 0;
-
+  $char;
+  $charCount;
 
   if (!$result = $mysqli->query($query)) {
     die('Invalid query: ' . $mysqli->error);
