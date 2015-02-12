ffcdd5bbe0138bd147c139c82e17dbea619ba413
logancollingwood <logan6694@gmail.com>
2015-02-12 11:44:14 -0800

    SearchBar work

--- techs/init.php
+++ techs/init.php
@@ -757,8 +757,8 @@ function sidebar($currentPage = '') {
               </div>';
 
                 if ($loggedIn) {
-                    echo "<div class='row'>";
-                      echo "<div class='loginbox'>";
+                    echo "<div class='loginbox'>";
+                      echo "<div class='row'>";
                       echo "    <hr class='login'>";
                       echo "<div class='col-md-6'>";
                         echo "    <a class='btn bttn login ";
@@ -774,9 +774,9 @@ function sidebar($currentPage = '') {
                     echo "</div>";
 
                   } else {
-                    echo "<div class='row'>";
-                      echo "<div class='loginbox'>";
+                    echo "<div class='loginbox'>";
                       echo "    <hr class='login'>";
+                      echo "<div class='row'>";
                       echo "<div class='col-md-6'>";
                         echo "    <a class='btn bttn login ";
                           if ($currentPage=='login') {
@@ -821,9 +821,15 @@ function navbar() {
       <a href="/" class="navbar-brand gccontroller"><img src="/img/assets/gccontroller.png" style="width:30px;height:20px;bottom:3px;"/></a>
     </div>
     <nav class="collapse navbar-collapse" role="navigation">
-    <form class="navbar-form navbar-left">
-        <div class="input-group input-group-sm" style="max-width:360px;">
+    <form class="navbar-form navbar-left search">
+        <div class="input-group input-group-sm" style="width:300px;">
           <input type="text" class="form-control" placeholder="Search" name="srch-term" id="srch-term">
+          <ul class="results" >
+            <li><a href="index.html">Search Result #1<br /><span>Description...</span></a></li>
+            <li><a href="index.html">Search Result #2<br /><span>Description...</span></a></li>
+            <li><a href="index.html">Search Result #3<br /><span>Description...</span></a></li>
+            <li><a href="index.html">Search Result #4</a></li>
+          </ul>
           <div class="input-group-btn">
             <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
           </div>
@@ -990,7 +996,7 @@ function printNewLibraries() {
   echo "<script src='https://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js'></script>";
   echo "<link href='//netdna.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css' rel='stylesheet'>";
   //echo "<link href='/css/new.css' rel='stylesheet'>";
-  echo "<link href='/css/users.css' rel='stylesheet'>";
+  //echo "<link href='/css/users.css' rel='stylesheet'>";
   //Open Graph Properties
   echo '
   <meta property="og:description" 
---
+++ techs/searchBar.php
@@ -0,0 +1,118 @@
+<?php
+	require_once("db.php");
+	$mysqli = new mysqli($dahostname, $username, $password, $database);
+
+	if ($mysqli->connect_errno) {   
+	  printf("Connect failed: %s\n", $mysqli->connect_error);
+	  exit();
+	}
+
+	if (!isset($_POST['query'])) {
+		echo "damn";
+		return;
+	}
+	// Get Search
+	$search_string = preg_replace("/[^A-Za-z0-9]/", " ", $_POST['query']);
+	$search_string = $mysqli->real_escape_string($search_string);
+	// Check Length More Than One Character
+	if (strlen($search_string) >= 1 && $search_string !== ' ') {
+
+		// Build Query
+		$query = 'SELECT users.username, userinfo.sponsor FROM users LEFT JOIN userinfo on users.id=userinfo.userid
+		WHERE users.username 
+		LIKE "%'.$search_string.'%" 
+		OR users.username LIKE "%'.$search_string.'%"';
+
+		if (!$result = $mysqli->query($query)) {
+		  die('Invalid query: ' . $mysqli->error);
+		}
+		foreach($result as $row) {
+		    $result_array[] = $row;
+		}
+
+		$query = 'SELECT techs.tech, techs.description FROM techs
+		WHERE techs.tech 
+		LIKE "%'.$search_string.'%" 
+		OR techs.tech LIKE "%'.$search_string.'%"';
+
+		if (!$result = $mysqli->query($query)) {
+		  die('Invalid query: ' . $mysqli->error);
+		}
+		foreach($result as $row) {
+		    $result_array[] = $row;
+		}
+
+
+		// Check If We Have Results
+		if (isset($result_array)) {
+		    foreach ($result_array as $result) {
+		        // Output
+        		$display_name = $result['username'];
+				$display_url = "/users/" . $result['username'];
+
+
+				// Insert Name
+				$output = str_replace('nameString', $display_name, $html);
+
+				// Insert Sponsor
+				if (isset($result['sponsor'])) {
+					$secondout = str_replace('no sponsor', $result['sponsor'], $output);
+				} else {
+					$secondout = str_replace('no sponsor', '', $output);
+				}
+
+				// Insert URL
+				$final = str_replace('urlString', $display_url, $secondout);
+
+				// Output
+				echo($final);
+		    }
+		} else {
+	    	// Format No Results Output
+
+			$output = str_replace('urlString', 'javascript:void(0);', $html);
+			$output = str_replace('<h4><small>no sponsor</small></h4>', '<br>', $html);
+			$output = str_replace('nameString', '<b>No Results Found.</b>', $output);
+			$output = str_replace('functionString', 'Sorry :(', $output);
+
+			// Output
+			echo($output);
+		}
+
+
+	} else {
+		$query = 'SELECT users.username, userinfo.sponsor FROM users LEFT JOIN userinfo on users.id=userinfo.userid';
+		if (!$result = $mysqli->query($query)) {
+		  die('Invalid query: ' . $mysqli->error);
+		}
+		foreach($result as $row) {
+		    $result_array[] = $row;
+		}
+
+		// Check If We Have Results
+		if (isset($result_array)) {
+		    foreach ($result_array as $result) {
+		        // Output
+        		$display_name = preg_replace("/".$search_string."/i", "<strong class='highlight'>".$search_string."</strong>", $result['username']);
+				$display_url = "/users/" . $result['username'];
+
+
+				// Insert Name
+				$output = str_replace('nameString', $display_name, $html);
+
+				// Insert Sponsor
+				if ($result['sponsor'] != '') {
+					$secondout = str_replace('no sponsor', $result['sponsor'], $output);
+				} else {
+					$secondout = str_replace('no sponsor', '', $output);
+				}
+
+				// Insert URL
+				$final = str_replace('urlString', $display_url, $secondout);
+
+				// Output
+				echo($final);
+		    }
+		}
+	}
+?>
