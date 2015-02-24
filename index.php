c22d6b97d30c3913b3167b5696e498ee398c2c6f
DannyPaguiao <dpaghari@ucsc.edu>
2015-02-23 21:57:36 -0800

    Added box shadows to EVERYTHING
    -Added subtle pattern for background
    -Added box shadow to panels
    -Added box shadow to buttons
    -Changed Random Giftainer size to 10 from 12

--- index.php
+++ index.php
@@ -96,7 +96,7 @@ Questions?
                           </div>
                         </div>
                         <div class="row">
-                          <div class="col-md-12 col-sm-12">
+                          <div class="col-md-10 col-sm-10 col-md-offset-1">
 
                             <?php
                               echo "<div id='randomSlot' class='panel panel-default gifTainer'>";
