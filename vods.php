ae29b77ea374d0a41023af0a20a3a5087d37dcb8
logancollingwood <logan6694@gmail.com>
2015-04-15 13:06:26 -0700

    Vods display

--- vods.php
+++ vods.php
@@ -38,154 +38,41 @@ Questions?
     	<!-- Main Dependencies -->
     	<?php printNewLibraries(); ?>
 
-    	<!-- Custom styles for this template -->
-    	<!-- <link href="css/dashboard_mobile.css" rel="stylesheet"> -->
-      <link href="css/rich.css" rel="stylesheet">
+      <link href="/css/less/rich.css" rel="stylesheet">
 
-
-    	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    	<!--[if lt IE 9]>
-      		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      		<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    	<![endif]-->
 
   	</head>
   	<body>
-  		<div class="wrapper">
+      <div class="wrapper">
         <div class="box">
-        <div class="row row-offcanvas row-offcanvas-left">
-                      
+
+          <!-- top nav -->
+          <?php navbar(); ?>
+          <!-- /top nav -->
+
+          <div class="row row-offcanvas row-offcanvas-left">
+
+          <?php sidebar('users'); ?>
+    
           
-            <?php sidebar('vods'); ?>
+           <!-- main right col -->
+          <div class="column col-md-10" id="main">
+            <section id="banner" data-speed="4" data-type="background">
+              <div class="jumbotron">
+                <div class='header'>vods</div>
+                <div class="blur">
 
-            <!-- main right col -->
-            <div class="column col-sm-10 col-xs-11" id="main">
-                
-                <!-- top nav -->
-                <div class="navbar navbar-sl navbar-static-top">  
-                    <div class="navbar-header">
-                      <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".navbar-collapse">
-                        <span class="sr-only">Toggle</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                      </button>
-                      <a href="/" class="navbar-brand gccontroller"><img src='/img/assets/gccontroller.png' style='width:30px;height:20px'/></a>
-                    </div>
-                    <nav class="collapse navbar-collapse" role="navigation">
-                    <form class="navbar-form navbar-left">
-                        <div class="input-group input-group-sm" style="max-width:360px;">
-                          <input type="text" class="form-control" placeholder="Search" name="srch-term" id="srch-term">
-                          <div class="input-group-btn">
-                            <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
-                          </div>
-                        </div>
-                    </form>
-                    <ul class="nav navbar-nav">
-                      <li>
-                        <a href="#"><i class="glyphicon glyphicon-home"></i> Home</a>
-                      </li>
-                      <li>
-                        <a href="#postModal" role="button" data-toggle="modal"><i class="glyphicon glyphicon-plus"></i> Post</a>
-                      </li>
-                      <li>
-                        <a href="#"><span class="badge">badge</span></a>
-                      </li>
-                    </ul>
-                    <ul class="nav navbar-nav navbar-right">
-                      <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="glyphicon glyphicon-user"></i></a>
-                        <ul class="dropdown-menu">
-                          <li><a href="">More</a></li>
-                          <li><a href="">More</a></li>
-                          <li><a href="">More</a></li>
-                          <li><a href="">More</a></li>
-                          <li><a href="">More</a></li>
-                        </ul>
-                      </li>
-                    </ul>
-                    </nav>
+                  <div class="description">Learn from some great, moderator curated vods</div>
+
+                  <div class="mast">
+                  </div>
                 </div>
-                <!-- /top nav -->
-              
-                <div class="padding">
-                    <div class="full col-sm-9">
-                      
-                        <!-- content -->   
-                        <div class="jumbotron banner">
-
-                          <h1 class='lead'>smash lounge</h1>
-                          <hr style='width:50%;'>
-                          <p class="fifty"><small>A compendium for acquiring ultimate smash knowledge. Check out an example page to get going</small></br></p>
-
-                        </div>
-                        <!--/row-->
-                      
-                        <div class="row">
-                          <div class="col-sm-6">
-                            <a href="#">Twitter</a> <small class="text-muted">|</small> <a href="#">Facebook</a> <small class="text-muted">|</small> <a href="#">Google+</a>
-                          </div>
-                        </div>
-                      
-                        <div class="row" id="footer">    
-                          <div class="col-sm-6">
-                            
-                          </div>
-                          <div class="col-sm-6">
-                            <p>
-                            <a href="#" class="pull-right">©Copyright 2013</a>
-                            </p>
-                          </div>
-                        </div>
-                      
-                      <hr>
-                      
-                      <h4 class="text-center">
-                      <a href="http://bootply.com/96266" target="ext">Download this Template @Bootply</a>
-                      </h4>
-                        
-                      <hr>
-                        
-                      
-                    </div><!-- /col-9 -->
-                </div><!-- /padding -->
+              </div>
+            </section>
+            <div class="content-wrapper">
             </div>
-            <!-- /main -->
-      </div>
-    </div>
-  </div>
-
-
-    <!--post modal-->
-    <div id="postModal" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
-      <div class="modal-dialog">
-      <div class="modal-content">
-          <div class="modal-header">
-              <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-          Update Status
           </div>
-          <div class="modal-body">
-              <form class="form center-block">
-                <div class="form-group">
-                  <textarea class="form-control input-lg" autofocus="" placeholder="What do you want to share?"></textarea>
-                </div>
-              </form>
-          </div>
-          <div class="modal-footer">
-              <div>
-              <button class="btn btn-primary btn-sm" data-dismiss="modal" aria-hidden="true">Post</button>
-                <ul class="pull-left list-inline"><li><a href=""><i class="glyphicon glyphicon-upload"></i></a></li><li><a href=""><i class="glyphicon glyphicon-camera"></i></a></li><li><a href=""><i class="glyphicon glyphicon-map-marker"></i></a></li></ul>
-          </div>  
-          </div>
+        </div>
       </div>
-      </div>
-    </div>
-    <script>
-      $(document).ready(
-        function() { 
-          $(".expander").niceScroll({cursorcolor:"#000", cursorborder:"0px solid #fff", cursorwidth: 10});
-        }
-      );
-    </script>
-  	</body>
+    </body>
 </html>
