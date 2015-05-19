97b6fe675b4d3ed65ccb82464f3a1f70dcc6a2bd
logancollingwood <logan6694@gmail.com>
2015-05-19 08:20:17 -0700

    404

--- js/pageJs/404.js
+++ js/pageJs/404.js
@@ -2,53 +2,64 @@
 $(document).ready(function(){
 
 	var animation = {
-
+		startDur: 6000,
+		moveRightDur: 1000,
+		delay: 3000,
+		resetSpeed: 1000,
 
 		start: function () {
+			//var duration = 3000;
+			//var moveRightDur = 2500;
+			//var delay = 3000;
+
 			var animateOpts = {
-			    duration: 3000,
+			    duration: this.startDur,
 			    complete: this.moveRight
 			};
-			
-			console.log(this);
-			$("#shot").animate({transform: "scale(1.3, 1.3)"}, animateOpts);
-			this.reset();
+
+			var transformStr = "scale(1.3, 1.3) rotate(10rad)";
+			console.log("start");
+			$("#shot").animate({transform: transformStr}, animateOpts);
+
+			setTimeout(this.reset, this.startDur + this.moveRightDur + this.delay);
+
+			//setTimeout(this.start, this.startDur + this.moveRightDur + this.delay + this.resetSpeed);
 		},
-		
+
+		moveRight: function () {
+			$("#samus").animate({transform: "rotate(-10deg)"}, 1);
+			console.log("right");
+			var animateOpts = {
+			    duration: this.moveRightDur
+			};
+			var distance = $("#scene").width() - 500;
+			var horizString = "translateX(" +  distance + ")";
+
+		    $("#shot").animate({transform: horizString}, animateOpts);
+		    
+		},
+
 		reset: function () {
 			console.log("resetting");
-			var distance = -$("#scene").width() - 1000;
-			var horizString = "translateX(" +  distance + ")";
-			console.log(horizString);
-			$("#shot").animate({transform: "scale(-1.3, -1.3)"}, 1);
-			$("#shot").animate({transform: horizString}, 1);
+
+
+			var optz = {
+				duration: this.resetSpeed
+			}
+			$("#samus").animate({transform: "rotate(0deg)"}, this.resetSpeed);
+			$("#shot").animate({transform: "scale(0.1, 0.1) "}, this.resetSpeed);
+			
 			
 		},
 		
-		moveRight: function () {
-			var animateOpts = {
-			    duration: 2500,
-			};
-			var distance = $("#scene").width() - 1000;
-			var horizString = "translateX(" +  distance + ")";
-			//console.log ("animating to " + horizString);
-		    $("#shot").animate({transform: horizString}, animateOpts);
-		},
+		
 
-		rotate: function () {
-			console.log("rotating");
-			var animateOpts = {
-			    duration: 1000
-			};
-			$("#shot").animate({transform: 'rotate(360deg)'}, animateOpts);
-		},
+	};
 
-	}
+	var optz =  {duration: 1};
+	$("#shot").animate({transform: "scale(0.1, 0.1)"}, optz);
 
-	//animation.reset();
-	animation.rotate();
 	animation.start();
 	
-	return animation;
 	
 });
